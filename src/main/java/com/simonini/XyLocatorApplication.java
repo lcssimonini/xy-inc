2d55a930bc1649d083403eaf91efca9c697aa306
simonini <simonini@simonini-Lenovo-ideapad-300-15ISK>
2017-01-21 10:32:04 -0200

    inicial

---
+++ src/main/java/com/simonini/XyLocatorApplication.java
@@ -0,0 +1,12 @@
+package com.simonini;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XyLocatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XyLocatorApplication.class, args);
+	}
+}
